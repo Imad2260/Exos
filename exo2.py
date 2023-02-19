55ee019c060e08a21e9ba7a3700d8baef835b8e0
Imad2260 <Imad2260@users.noreply.github.com>
2023-02-19 19:30:51 +0000

    Delete exo2.py

--- exo2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-f = open("data.txt", "r")
-content_data = f.read()
-f.close()
-##############################################################
-
-resultat = re.findall(r"[a-zA-Z]+[\s]", content_data)
-print(resultat)
