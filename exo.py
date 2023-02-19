5bf966aed9b2bb3fe62b44663a534b42cb5f4c3c
Imad2260 <Imad2260@users.noreply.github.com>
2023-02-19 19:30:41 +0000

    Delete exo.py

--- exo.py
+++
@@ -1,9 +0,0 @@
-import re
-
-f = open("domains.xml", "r")
-content_domains = f.read()
-f.close()
-#########################################################
-
-resultat = re.findall(r"\.[a-z]{2,4}[\|<|\n]", content_domains)
-print(resultat)
